080f815c0d376dccc1e65c2c1b465c3606670c25
hackritik000 <hackritik000@gmail.com>
2025-09-02 15:46:07 +0530

    adding

--- apps/web/src/app/form/page.tsx
+++ apps/web/src/app/form/page.tsx
@@ -1,23 +1,89 @@
-import { FormProvider, useForm } from "react-hook-form";
-import { InputField } from "@/components/form/reuseInput";
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  emptyToNull,
+  emptyToUndefined,
+  type FieldDataType,
+  FieldType,
+  FormGenerator,
+} from "@/components/form-generator";
+import { Button } from "@/components/ui/button";
+
+const formFields: FieldDataType[] = [
+  {
+    name: "name",
+    label: "Name",
+    type: FieldType.Text,
+    default: "",
+    required: true,
+    schema: z.preprocess(emptyToUndefined, z.string()),
+  },
+  {
+    name: "department",
+    label: "Department",
+    type: FieldType.Select,
+    default: undefined,
+    required: true,
+    schema: z.string(),
+    options: ["IT", "Sales", "Marketing", "Finance"],
+  },
+  {
+    name: "salary",
+    label: "Salary",
+    type: FieldType.Number,
+    default: 0,
+    required: false,
+    schema: z.preprocess(emptyToNull, z.number().nullable()),
+  },
+  {
+    name: "isActive",
+    label: "Is Active",
+    type: FieldType.Checkbox,
+    default: true,
+    required: false,
+    schema: z.boolean(),
+  },
+  {
+    name: "dateOfEmployment",
+    label: "Date of Employment",
+    type: FieldType.DatePicker,
+    default: new Date(),
+    required: true,
+    schema: z.date(),
+  },
+  {
+    name: "notes",
+    label: "Notes",
+    type: FieldType.Textarea,
+    default: "",
+    required: false,
+    schema: z.preprocess(emptyToNull, z.string().nullable()),
+  },
+] as const;
 
 export default function MyForm() {
-  const methods = useForm();
+  const formGenerator = new FormGenerator<typeof formFields>(formFields);
+
+  const schema = z.object(formGenerator.schema);
+  type FormSchemaType = z.infer<typeof schema>;
+
+  const defaultValues = formGenerator.defaultValues;
+
+  const form = useForm<FormSchemaType>({
+    resolver: zodResolver(schema),
+    defaultValues: defaultValues,
+  });
+
+  const onSubmit = (data: FormSchemaType) => {
+    console.log(data);
+  };
+
   return (
-    <FormProvider {...methods}>
-      <InputField
-        name="name"
-        label="Name"
-        placeholder="Enter your name"
-        required="Name is required"
-      />
-      <InputField
-        name="email"
-        label="Email"
-        placeholder="Enter your email"
-        type="email"
-        required="Email is required"
-      />
-    </FormProvider>
+    <form onSubmit={form.handleSubmit(onSubmit)}>
+      {formGenerator.fields(form)}
+      <Button type="submit">Submit</Button>
+    </form>
   );
 }
