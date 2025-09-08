9b91049d1af714e880dff7067f0247b2367abd3f
itzmeranjeet <ranjeetchauhan212000@gmail.com>
2025-09-08 12:45:52 +0530

    add dashboard ..

--- apps/dashboard/src/dashboardold/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Skeleton } from "@/components/ui/skeleton";
-
-export default function Loading() {
-  return (
-    <div className="flex w-full h-[100vh] bg-white justify-center items-center">
-      <Skeleton className="w-96 h-96" />
-    </div>
-  );
-}
--- apps/dashboard/src/dashboardold/app/menu-card/components/product-card.tsx
+++
@@ -1,7 +0,0 @@
-export default function ProductCardComponent() {
-  return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-3xl"></div>
-    </div>
-  );
-}
