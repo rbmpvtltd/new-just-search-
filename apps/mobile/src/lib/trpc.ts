d5d02f66b85d08bfa1643ec7dd8bef7287187107
meekailkhan <meekailkhan123aar@gmail.com>
2025-09-12 11:00:57 +0530

    solve error of netword request failed

--- apps/mobile/src/lib/trpc.ts
+++ apps/mobile/src/lib/trpc.ts
@@ -28,8 +28,8 @@ export const trpc = createTRPCOptionsProxy<AppRouter>({
 
 function getTrpcUrl() {
   const base = (() => {
-    if (process.env.URL) return `https://${process.env.URL}`;
-    return "http://localhost:4000";
+    // if (process.env.URL) return `https://${process.env.URL}`;
+    return "http://192.168.1.35:4000";
   })();
   return `${base}/trpc`;
 }
