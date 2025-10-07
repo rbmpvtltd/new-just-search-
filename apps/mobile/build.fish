1d4fcbffb84ac87b70f1c088400be722e54faa10
hackritik000 <hackritik000@gmail.com>
2025-10-07 16:20:50 +0530

    fixing build script

--- apps/mobile/build.fish
+++ apps/mobile/build.fish
@@ -2,21 +2,24 @@
 
 # Function to clean temp and gradle files
 function cleanup
-
     echo (set_color yellow)"Setting java version 17"(set_color normal)
     echo (set_color yellow)"Enter your password"(set_color normal)
     java-17
     echo (set_color yellow)"[INFO] Cleaning temp build files..."(set_color normal)
 
-    # cd $mydir
-    cp -r ./apps/mobile ~/eas-build-tmp
+    set tmpdir ~/eas-build-tmp
+    if not test -d $tmpdir
+        mkdir -p $tmpdir
+        echo (set_color green)"[OK] Created temp directory $tmpdir."(set_color normal)
+    end
 
-    cd ~/eas-build-tmp/tmp
+    cd $tmpdir
+
     if test (count *) -gt 0
         rm -rf *
         echo (set_color green)"[OK] Temp directory cleared."(set_color normal)
     else
-        echo (set_color red)"[SKIP] Temp directory already empty."(set_color normal)
+        echo (set_color blue)"[SKIP] Temp directory already empty."(set_color normal)
     end
     cd ~
 
@@ -24,7 +27,7 @@ function cleanup
         rm -rf .gradle
         echo (set_color green)"[OK] Removed .gradle."(set_color normal)
     else
-        echo (set_color red)"[SKIP] .gradle not found."(set_color normal)
+        echo (set_color blue)"[SKIP] .gradle not found."(set_color normal)
     end
 end
 
@@ -45,6 +48,7 @@ end
 
 set platform $argv[1]
 set profile $argv[2]
+
 set mydir (pwd)
 
 # Validate platform
@@ -54,9 +58,8 @@ switch $platform
         switch $profile
             case development preview production
                 cleanup
-                cd ~/eas-build-tmp/mobile
-                yarn install
-                TMPDIR=~/eas-build-tmp/tmp eas build --platform $platform --clear-cache --local --profile=$profile
+                cd $mydir
+                TMPDIR=~/eas-build-tmp eas build --platform $platform --clear-cache --local --profile=$profile
                 java-24
             case '*'
                 usage
