2b3fecab257ab420e88f5e12bff43a26d01c7566
meekailkhan <meekailkhan123aar@gmail.com>
2025-12-20 16:48:44 +0530

    working on notification

--- apps/backend/src/features/notification/notification.router.ts
+++ apps/backend/src/features/notification/notification.router.ts
@@ -1,7 +1,34 @@
 import { protectedProcedure, publicProcedure, router } from "@/utils/trpc";
+import { db } from "@repo/db";
+import {
+  pushTokenInsertSchema,
+  pushTokens,
+} from "@repo/db/dist/schema/notification.schema";
 
 export const notificationRouter = router({
   getNotification: protectedProcedure.query(async ({ ctx }) => {
     return { success: true, data: ctx };
   }),
+  createPushToken: protectedProcedure
+    .input(pushTokenInsertSchema)
+    .mutation(async ({ input, ctx }) => {
+      const userId = ctx.userId;
+      const { token, deviceId, platform } = input;
+
+      await db
+        .insert(pushTokens)
+        .values({
+          userId,
+          token,
+          deviceId,
+          platform,
+        })
+        .onConflictDoUpdate({
+          target: [pushTokens.deviceId],
+          set: {
+            token,
+            lastActiveAt: new Date(),
+          },
+        });
+    }),
 });
