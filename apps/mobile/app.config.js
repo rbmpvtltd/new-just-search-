5a92ed9e18462185a665d64e43be77faf67c9417
hackritik000 <hackritik000@gmail.com>
2025-12-22 15:12:37 +0530

    remove keyboard

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -1,5 +1,16 @@
 export default ({ config }) => {
   return {
     ...config,
+    ios: {
+      ...config.ios,
+      googleServicesFile: process.env.GOOGLE_SERVICE_PLIST,
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false,
+      },
+    },
+    android: {
+      ...config.android,
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+    },
   };
 };
