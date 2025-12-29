ec853fd1cc59063c4fccfe76e2d90c94f825cf5f
meekailkhan <meekailkhan123aar@gmail.com>
2025-12-29 16:10:46 +0530

    change credentail

--- apps/mobile/src/app/(root)/google/index.tsx
+++ apps/mobile/src/app/(root)/google/index.tsx
@@ -1,26 +1,45 @@
+import { trpc } from "@/lib/trpc";
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import { useMutation } from "@tanstack/react-query";
 import { Pressable, Text, View } from "react-native";
 
 GoogleSignin.configure({
   webClientId:
-    "968676221050-brhretmcqmd0pavpus0rg38vall2fbfp.apps.googleusercontent.com",
+    "571980400521-es8jn3ged1mu8elmrpei5p765isacrn7.apps.googleusercontent.com",
   iosClientId:
     "571980400521-nd45lmn71u436lbj62gi5t7i82osuve1.apps.googleusercontent.com",
 });
 
 const GoogleLogin = () => {
-  async function onSumbmit() {
+  const { mutate } = useMutation(trpc.auth.mobileOauth.mutationOptions());
+  async function onClick() {
     console.log("start login with google");
     await GoogleSignin.hasPlayServices();
     const userInfo = await GoogleSignin.signIn();
     console.log(userInfo);
+    mutate(
+      {
+        idToken: userInfo.data?.idToken ?? "",
+        provider: "google",
+        user: {
+          email: userInfo.data?.user.email ?? "",
+          name: userInfo.data?.user.givenName ?? "",
+          id: userInfo.data?.user.id ?? "",
+        },
+      },
+      {
+        onSuccess: (data) => {
+          console.log("==============>", data);
+        },
+        onError: (error) => {
+          console.log("==============>", error);
+        },
+      },
+    );
   }
   return (
     <View className="flex-1 justify-center items-center">
-      <Pressable
-        onPress={onSumbmit}
-        className="bg-primary text-white py-4 px-5"
-      >
+      <Pressable onPress={onClick} className="bg-primary text-white py-4 px-5">
         <Text className="text-secondary">Login With Google</Text>
       </Pressable>
     </View>
