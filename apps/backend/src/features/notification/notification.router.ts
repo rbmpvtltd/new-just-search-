6a5aae921aab401b863c4f7928c7e8ed245d0dc7
meekailkhan <meekailkhan123aar@gmail.com>
2026-01-06 17:52:55 +0530

    get notification listing

--- apps/backend/src/features/notification/notification.router.ts
+++ apps/backend/src/features/notification/notification.router.ts
@@ -1,35 +1,54 @@
 import { protectedProcedure, publicProcedure, router } from "@/utils/trpc";
 import { db } from "@repo/db";
 import { users } from "@repo/db/dist/schema/auth.schema";
-import { businessCategories, businessListings, businessSubcategories } from "@repo/db/dist/schema/business.schema";
+import {
+  businessCategories,
+  businessListings,
+  businessSubcategories,
+} from "@repo/db/dist/schema/business.schema";
 import {
   pushTokenInsertSchema,
   pushTokens,
 } from "@repo/db/dist/schema/notification.schema";
 import { notification } from "@repo/db/dist/schema/user.schema";
 import { TRPCError } from "@trpc/server";
-import {and, eq, or} from "drizzle-orm"
+import { and, eq, inArray, or } from "drizzle-orm";
 
 export const notificationRouter = router({
-  getNotifications : protectedProcedure.query(async({ctx})=>{
-    if(ctx.role === "business"){
-      const data = await db.select({
-        title: notification.title,
-        description: notification.description,
-        id : notification.id,
-        categoryId : businessCategories.categoryId,
-        subcategory : businessSubcategories.subcategoryId
-      })
-      .from(notification)
-      .leftJoin(
-        businessListings,eq(businessListings.userId,ctx.userId)
-      )
-      .leftJoin(businessCategories,eq(businessCategories.businessId,businessListings.id))
-      .leftJoin(businessSubcategories,eq(businessSubcategories.businessId,businessListings.id))
-      .where(or(eq(notification.categoryId,businessCategories.categoryId),eq(notification.subCategoryId,businessSubcategories.subcategoryId)))
-      .groupBy(businessCategories.id,notification.id,businessSubcategories.id)
-  
-      return { success : true,data : data,ctx}
+  getNotifications: protectedProcedure.query(async ({ ctx }) => {
+    if (ctx.role === "business") {
+      const listings = await db
+        .select({
+          id : businessListings.id,
+          city : businessListings.city,
+          state : businessListings.state,
+          name : businessListings.name
+        })
+        .from(businessListings)
+        .where(eq(businessListings.userId, ctx.userId));
+
+      const categories = await db
+        .select()
+        .from(businessCategories)
+        .where(
+          inArray(
+            businessCategories.businessId,
+            listings.map((l) => l.id),
+          ),
+        );
+
+      const subcategories = await db
+        .select()
+        .from(businessSubcategories)
+        .where(
+          inArray(
+            businessSubcategories.businessId,
+            listings.map((l) => l.id),
+          ),
+        );
+
+      const data = await db.select().from(notification).where(or(eq(notification.city,listings[0]?.city ?? 0),eq(notification.state,listings[0]?.state ?? 0)))
+      return { success: true, data: ctx,listings,categories,subcategories };
     }
   }),
   createPushToken: protectedProcedure
@@ -88,5 +107,4 @@ export const notificationRouter = router({
         });
       }
     }),
-
 });
