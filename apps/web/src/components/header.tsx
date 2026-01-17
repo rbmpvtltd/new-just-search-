72a87fdfb0f3b960686bcd4dfcb0ee81327cbac5
hackritik000 <hackritik000@gmail.com>
2026-01-17 17:58:39 +0530

    fixing header

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -19,13 +19,21 @@ export const HeroHeader = () => {
   const data = Cookies.get("authenticated");
 
   return (
-    <header className="sticky top-0 z-50">
+    <header className="sticky top-0 z-10">
       <nav
         data-state={menuState && "active"}
-        className="bg-background/50 z-20 w-full border-b backdrop-blur-3xl"
+        className="bg-background/50 w-full border-b backdrop-blur-3xl"
       >
         <div className="mx-auto max-w-6xl px-6 transition-all duration-300">
           <div className="relative flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4">
+            {menuState && (
+              <button
+                onClick={() => setMenuState(false)}
+                type="button"
+                className="absolute top-0 z-20 left-[-50vw] h-[120vh] w-[200vw] bg-gray-500 opacity-30"
+              ></button>
+            )}
+
             <div className="flex w-full items-center justify-between gap-12 lg:w-auto">
               <Link
                 href="/"
@@ -45,7 +53,7 @@ export const HeroHeader = () => {
                 onClick={() => setMenuState(!menuState)}
                 type="button"
                 aria-label={menuState === true ? "Close Menu" : "Open Menu"}
-                className="relative z-20 -m-2.5 -mr-4 block cursor-pointer p-2.5 lg:hidden"
+                className="relative -m-2.5 -mr-4 block cursor-pointer p-2.5 lg:hidden"
               >
                 <Menu className="in-data-[state=active]:rotate-180 in-data-[state=active]:scale-0 in-data-[state=active]:opacity-0 m-auto size-6 duration-200" />
                 <X className="in-data-[state=active]:rotate-0 in-data-[state=active]:scale-100 in-data-[state=active]:opacity-100 absolute inset-0 m-auto size-6 -rotate-180 scale-0 opacity-0 duration-200" />
@@ -69,7 +77,7 @@ export const HeroHeader = () => {
               </div>
             </div>
 
-            <div className="bg-background absolute lg:static top-[calc(100%+1em)] w-full border-bl in-data-[state=active]:block lg:in-data-[state=active]:flex mb-6 hidden flex-wrap items-center justify-end space-y-8 rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none dark:shadow-none dark:lg:bg-transparent">
+            <div className="bg-background absolute z-50 lg:static top-[calc(100%+1em)] w-full border-bl in-data-[state=active]:block lg:in-data-[state=active]:flex mb-6 hidden flex-wrap items-center justify-end space-y-8 rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none dark:shadow-none dark:lg:bg-transparent">
               <div className="lg:hidden">
                 <ul className="space-y-6 text-base">
                   {menuItems.map((item, index) => (
