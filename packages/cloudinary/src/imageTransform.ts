af3725819b988289e0fceab661eed22bd465ecb5
hackritik000 <hackritik000@gmail.com>
2026-01-27 11:35:47 +0530

    working on transform

--- packages/cloudinary/src/imageTransform.ts
+++ packages/cloudinary/src/imageTransform.ts
@@ -2,14 +2,9 @@ import sharp from "sharp";
 
 export async function urlToWebP(url: string, quality = 80) {
   const res = await fetch(url.trim());
-  const outputPath = url.split("/").pop()?.split(".")[0];
+  const outputName = url.split("/").pop()?.split(".")[0];
   if (!res.ok) throw new Error(`HTTP ${res.status}: ${res.statusText}`);
   const imageBytes = await res.bytes();
-  sharp(imageBytes).webp({ quality }).toFile(`public/${outputPath}.webp`);
-  return `${outputPath}.webp`;
+  sharp(imageBytes).webp({ quality }).toFile(`public/${outputName}.webp`);
+  return `${outputName}.webp`;
 }
-
-const imageUrl =
-  "https://justsearch.net.in/assets/images/banners/KOnKsjMj1760704626.png  ";
-
-console.log(await urlToWebP(imageUrl));
